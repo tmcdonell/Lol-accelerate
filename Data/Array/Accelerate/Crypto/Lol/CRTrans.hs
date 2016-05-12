e88814fb6d677fff7801d86965557eb1c8fa9b7a
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-05-12 17:28:39 +1000

    CRTrans instances for Exp types

--- Data/Array/Accelerate/Crypto/Lol/CRTrans.hs
+++ Data/Array/Accelerate/Crypto/Lol/CRTrans.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE PolyKinds             #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -15,22 +18,74 @@
 
 module Data.Array.Accelerate.Crypto.Lol.CRTrans (
 
+  CRTrans(..),
   CRTEmbed(..)
 
 ) where
 
-import Data.Array.Accelerate                                        as A
+import Data.Array.Accelerate                                        as A hiding ( (*), (/) )
 import qualified Data.Array.Accelerate.Algebra.Additive             as Additive ()
 import qualified Data.Array.Accelerate.Algebra.Ring                 as Ring ()
 import qualified Data.Array.Accelerate.Algebra.Transcendental       as Transcendental ()
 
 import Data.Array.Accelerate.Crypto.Lol.Types.Complex               as A
+import Data.Array.Accelerate.Crypto.Lol.Reflects
 
 import Crypto.Lol.Cyclotomic.Tensor.Accelerate.AT
 
 import Crypto.Lol.CRTrans
 import Crypto.Lol.LatticePrelude                                    as LP
 
+
+-- CRTrans
+-- -------
+
+-- CRTrans for product rings
+--
+instance (CRTrans monad (Exp i) (Exp a), CRTrans monad (Exp i) (Exp b), Elt a, Elt b)
+    => CRTrans monad (Exp i) (Exp (a,b)) where
+  crtInfo = do
+    (fa, inva :: Exp a) <- crtInfo
+    (fb, invb :: Exp b) <- crtInfo
+    return (\i -> A.lift (fa i, fb i), A.lift (inva, invb))
+
+-- Complex numbers have roots of unity of any order
+--
+instance (Monad monad, Transcendental (Exp a), A.Num a, A.FromIntegral Int a, Elt a)
+    => CRTrans monad (Exp Int) (Exp (Complex a)) where
+  crtInfo = crtInfoC
+
+crtInfoC
+    :: forall monad m a. (Monad monad, Reflects m (Exp Int), Transcendental (Exp a), A.Num a, A.FromIntegral Int a, Elt a)
+    => TaggedT m monad (CRTInfo (Exp Int) (Exp (Complex a)))
+crtInfoC =
+  let
+      mval = proxy value (Proxy::Proxy m)
+      mhat = let (d,m) = LP.divMod mval 2 -- this is @valueHat mval@ lifted to Exp
+             in  m ==* 0 ? ( d, mval )
+  in
+  return ( omegaPowC mhat
+         , LP.recip (A.fromReal (A.fromIntegral mhat))
+         )
+
+omegaPowC
+    :: (Transcendental (Exp a), A.Num a, A.FromIntegral Int a, Elt a)
+    => Exp Int
+    -> Exp Int
+    -> Exp (Complex a)
+omegaPowC m i = A.cis (2 * LP.pi * A.fromIntegral i / A.fromIntegral m)
+
+-- CRTrans instances for real and integer types, which do not have roots of
+-- unity (except in trivial cases).
+--
+instance CRTrans Maybe (Exp Int) (Exp Double) where crtInfo = tagT Nothing
+instance CRTrans Maybe (Exp Int) (Exp Int)    where crtInfo = tagT Nothing
+instance CRTrans Maybe (Exp Int) (Exp Int64)  where crtInfo = tagT Nothing
+
+
+
+-- CRTEmbed
+-- --------
 
 -- Instance for product rings
 instance (CRTEmbed AT a, CRTEmbed AT b, Elt a, Elt b, Elt (CRTExt a), Elt (CRTExt b))
